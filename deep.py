a481baba565a069a7ae37443f7b9868b095be52b
Gojo455 <davidayomide092@gmail.com>
2026-09-18 23:17:29 +0100

    a python math interpreeter that takes math expressions literally and solves them

--- deep.py
+++ deep.py
@@ -1,13 +1,14 @@
 """
-In deep.py, implement a program that 
+In deep.py, implement a program that
 prompts the user for the answer to the
-Great Question of Life, the Universe 
-and Everything, outputting Yes if the user 
+Great Question of Life, the Universe
+and Everything, outputting Yes if the user
 inputs 42 or (case-insensitively) forty-two
 or forty two. Otherwise output No."""
 # ask user for input, then lower it so it matches the conditions
 entry = input("Answer to the Great question: ")
 entry = entry.lower()
+entry = entry.strip()
 
 if entry in ("42", "forty-two", "forty two"):
 # the in key word only accepts strings on the left side
---
+++ interpreter/interpreter.py
@@ -0,0 +1,11 @@
+expr = input("Expression: ")
+for char in expr:
+    if char in "+-*/":
+        operator = char
+        x = int(expr[:char])
+        y = int(expr[char+1:])
+        x, y = x.replace(" ",""), y.replace(" ","")
+
+if operator = 
+        
+
